f6352119e7a1e311d6e8d51cdc87670a0533a584
vineethkumar1703vk <nnm22ec205@nmamit.in>
2025-07-31 15:20:03 +0530

    small change

--- vite.config.js
+++ vite.config.js
@@ -23,5 +23,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/resumeWebsite/',
   plugins: [tailwindcss(), react()],
 })
